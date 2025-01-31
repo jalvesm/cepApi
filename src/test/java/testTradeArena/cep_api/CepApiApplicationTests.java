aedad8ea4b1657866ebc24de6520c0128444920c
Joana Morais <joanaalvesm@gmail.com>
2025-01-31 12:17:38 -0300

    Spring Boot project template for CEP API.

---
+++ src/test/java/testTradeArena/cep_api/CepApiApplicationTests.java
@@ -0,0 +1,13 @@
+package testTradeArena.cep_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
